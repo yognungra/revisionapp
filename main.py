d62f7206b54c4f7f418eb56e8322b4b650a3d762
yognungra <yognungra@gmail.com>
2025-07-07 10:57:59 +0000

    sqlite

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Hello World")
+import sqlite3
+
+connection = sqlite3.connect("Users.db")
+c = connection.cursor()
+print(c)
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS
+Users (
+        UserID INTEGER PRIMARY KEY NOT NULL,
+        FirstName TEXT NOT NULL,
+        LastName TEXT NOT NULL,
+        Email TEXT NOT NULL,
+        PasswordHarsh TEXT NOT NULL,
+        UserRole TEXT NOT NULL,
+        SchoolID INTEGER NOT NULL,
+        DateCreated INTEGER NOT NULL
+            )
+          """)
+
+
+def log_in():
+    existing_user = input("Are you an existing user Y/N: ")
+    if existing_user.lower() == "y":
+        existing_user = True
+    elif existing_user.lower() == "n":
+        existing_user = False
+
+    if existing_user:
+        email = input("Email: ")
+        password = input("Password: ")
+
+    
