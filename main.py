1673eab43fe3f3830d32f07b25cec89812e47cda
yognungra <yognungra@gmail.com>
2025-07-24 15:37:47 +0000

    end1

--- main.py
+++ main.py
@@ -29,24 +29,36 @@ def hash(input_text):
     hashed = ph.hash("input_text")
     return hashed
 
-c.execute("INSERT INTO Users(FirstName, LastName, Email, PasswordHash, UserRole, SchoolID) VALUES(?, ?, ?, ?, ?, ?);", ("Yog", "Nungra", "yognungra@gmail.com", ph.hash("yognungra"), "Student", 1))
+#c.execute("INSERT INTO Users(FirstName, LastName, Email, PasswordHash, UserRole, SchoolID) VALUES(?, ?, ?, ?, ?, ?);", ("Yog", "Nungra", "yognungra@gmail.com", ph.hash("yognungra"), "Student", 1))
 connection.commit()
 
 def log_in():
-    existing_user = input("Are you an existing user Y/N: ")
-    if existing_user.lower() == "y":
-        existing_user = True
-    elif existing_user.lower() == "n":
-        existing_user = False
+    valid_option = False
+    while not valid_option:
+        existing_user = input("Are you an existing user Y/N: ")
+        if existing_user.lower() == "y" or existing_user.lower() == "yes":
+            existing_user = True
+            valid_option = True
+        elif existing_user.lower() == "n" or existing_user.lower() == "no":
+            existing_user = False
+            valid_option = True
+        else:
+            print("Invalid Option! Try again.")
 
     if existing_user:
-        email = input("Email: ")
-        password = input("Password: ")
 
-        hash = ph.hash(password)
 
-        databaseHash = c.execute("SELECT passwordHash FROM Users WHERE email=?", (email,))
 
-        print(ph.verify(hash, databaseHash))
+        verified = False
+
+        while not verified:
+            email = input("Email: ")
+            password = input("Password: ")
+            databaseHash = c.execute("SELECT passwordHash FROM Users WHERE email=?", (email,))
+            data = databaseHash.fetchone()
+            try:
+                print(ph.verify(data[0], password))
+            except Exception as e:
+                print("ERROR:", e)
 
 log_in()
